8d957f8512636f5f2233ea3f14f39fbada1aac38
Chase Van Halen <cvanhalen@perscholas.org>
2023-01-20 14:41:01 -0600

    finish project

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const mongoose = require('mongoose')
 require('dotenv').config()
+let Article = require('./Models/article.js')
 
 const app = express()
 
 app.use(express.static('public'));
 
-const connectionString = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@mongosetupcluster.6pmvhu8.mongodb.net/WebsiteContent/?retryWrites=true&w=majority`
+// parse req.body for use
+app.use(express.json());
+
+// parse queries from url
+app.use(express.urlencoded({extended: true}))
+
+console.log(new Date());
+
+const connectionString = `mongodb+srv://${process.env.MONGOUSERNAME}:${process.env.PASSWORD}@mongosetupcluster.6pmvhu8.mongodb.net/WebsiteContent?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false);
 // connect to our MongoDB database (our Models specify which collections)
@@ -18,6 +27,16 @@ mongoose.connect(connectionString, {
 mongoose.connection.once('open', ()=> {
     console.log('connected to mongo');
 });
+
+
+// make a route that accepts some info and makes a document in the collection
+
+app.post("/create_article", (req, res) => {
+    console.log(req.body);
+    Article.create(req.body);
+    res.send("good request")
+})
+
 
 app.listen(5000, () => {
     console.log(`Server is Listening on 5000`)
